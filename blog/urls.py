6674319e6c05d4bc77d933b6289351ff0303cde7
mehrozmalik <mehrozmalik7@gmail.com>
2021-09-04 01:09:17 +0500

    move code

---
+++ blog/urls.py
@@ -0,0 +1,19 @@
+from django.urls import path
+from django.views.generic.list import ListView
+from .feeds import LatestPostFeed
+from . import views
+
+app_name = 'blog'
+
+
+urlpatterns = [
+    # path('', views.PostListView.as_view(), name='post_list'),
+    path('', views.post_list, name='post_list'),
+    path('<int:year>/<int:month>/<int:day>/<slug:post>/',
+         views.post_detail, name='post_detail'),
+    path('<int:post_id>/share/', views.post_share, name='post_share'),
+    path('tag/<slug:tag_slug>/', views.post_list, name='post_list_by_tag'),
+    path('feed/', LatestPostFeed(), name='post_feed'),
+]
+
+# page 110
